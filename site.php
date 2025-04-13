2db56fde8bc4fb84d4885e731089c7f0831df42f
2405000556 <scottivanm12@gmail.com>
2025-04-13 10:06:16 +0200

    Add files via upload

---
+++ site.php
@@ -0,0 +1,17 @@
+<!doctyp html>
+<head></head>
+<body>
+<h1>my first project on php</h1>
+<?php
+
+for($x=1; $x<=6; $x++){
+    echo " the product is :$x * 6 = " . ($x * 6) . "<br>";
+    echo"\n";
+}
+?>
+</body>
+</html>
+
+
+
+
